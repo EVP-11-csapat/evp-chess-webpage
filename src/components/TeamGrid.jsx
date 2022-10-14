52b147452b21b91286e436a6a2820f7308ee542d
Nemeth-Tamas <tamas.nemeth2002@gmail.com>
2022-10-14 14:56:04 +0200

    Added Organization to team page

--- src/components/TeamGrid.jsx
+++ src/components/TeamGrid.jsx
@@ -8,6 +8,7 @@ const TeamGrid = () => {
     const team = {
         "members": [
             {
+                "id": "1",
                 "name": "Németh Tamás",
                 "email": "swtjxf@inf.elte.hu",
                 "links": [
@@ -18,6 +19,7 @@ const TeamGrid = () => {
                 ]
             },
             {
+                "id": "2",
                 "name": "Riszterer István",
                 "email": "gwlxts@inf.elte.hu",
                 "links": [
@@ -27,7 +29,8 @@ const TeamGrid = () => {
                     }
                 ]
             },
-            {
+            {   
+                "id": "3",
                 "name": "Maár Dávid",
                 "email": "ssqod1@inf.elte.hu",
                 "links": [
@@ -37,14 +40,25 @@ const TeamGrid = () => {
                     }
                 ]
             },
+            {   
+                "id": "4",
+                "name": "11. Csapat",
+                "links": [
+                    {
+                        "name": "GitHub Organization",
+                        "url": "https://github.com/EVP-11-csapat"
+                    }
+                ]
+            },
         ]
     }
 
     return (
         <div>
             <Container>
-                <Row>
-                    {team.members.map((member) => (
+                <Row className="justify-content-lg-center">
+                    {team.members.map((member) =>
+                        {if (member.id <= 3) return (
                         <Col lg={4} md={12} style={{marginBottom: '0.5rem'}}>
                             <Card className="bg-dark text-danger border-primary">
                                 <Card.Body>
@@ -63,7 +77,30 @@ const TeamGrid = () => {
                                 </Card.Body>
                             </Card>
                         </Col>
-                    ))}
+                    )})}
+                </Row>
+                <Row className="justify-content-lg-center">
+                    {team.members.map((member) =>
+                        {if (member.id > 3 && member.id <=6) return (
+                        <Col lg={4} md={12} style={{marginBottom: '0.5rem'}}>
+                            <Card className="bg-dark text-danger border-primary">
+                                <Card.Body>
+                                    <Card.Title style={{fontSize: '2rem'}}>{member.name}</Card.Title>
+                                    <Card.Subtitle style={{marginBottom: '0.3rem', fontSize: '1.1rem'}}>
+                                        {/* <Card.Link 
+                                            className="mb-2 text-muted" 
+                                            style={{textDecoration: 'none'}}
+                                            href={"mailto:" + member.email}>
+                                            <FontAwesomeIcon icon={faEnvelope} style={{marginRight: "0.2rem"}} />: {member.email}
+                                        </Card.Link> */}
+                                    </Card.Subtitle>
+                                    {member.links.map((link) => (
+                                        <Card.Link href={link.url} target="_blank" style={{textDecoration: 'none', fontSize: '1.1rem'}}>{link.name.includes("GitHub") ? (<FontAwesomeIcon icon={faGithub} style={{marginRight: "0.3rem"}} />) : ""}{link.name}</Card.Link>
+                                    ))}
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    )})}
                 </Row>
             </Container>
         </div>
