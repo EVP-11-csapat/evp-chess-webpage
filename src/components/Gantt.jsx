8b7e577063056947cfa0293a9fcbcdf4828f0aa1
Nemeth-Tamas <tamas.nemeth2002@gmail.com>
2022-11-04 14:35:06 +0100

    New PDF and Gantt

--- src/components/Gantt.jsx
+++ src/components/Gantt.jsx
@@ -4,7 +4,7 @@ import { Container, Image } from 'react-bootstrap'
 const Gantt = () => {
   return (
     <Container>
-      <Image src='https://i.imgur.com/LcKayAO.png' fluid />
+      <Image src='https://i.imgur.com/OZqYQRN.png' fluid />
     </Container>
   )
 }
