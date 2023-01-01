bbe240657c9637715fab110e5dfa0d801a7f13d6
Németh Tamás <34923997+Nemeth-Tamas@users.noreply.github.com>
2023-01-01 10:48:50 +0100

    Added final Gantt

--- src/components/Gantt.jsx
+++ src/components/Gantt.jsx
@@ -4,7 +4,10 @@ import { Container, Image } from 'react-bootstrap'
 const Gantt = () => {
   return (
     <Container>
-      <Image src='https://i.imgur.com/OZqYQRN.png' fluid />
+      <a href='https://i.imgur.com/2y0EVO3.png' target="_blank" rel='noreferrer'>
+        <Image src='https://i.imgur.com/2y0EVO3.png' fluid />
+      </a>
+      <h6 className='text-danger'>Click on the picture to open the original</h6>
     </Container>
   )
 }
